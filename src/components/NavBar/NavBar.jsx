7c2b3efe73cc7eeb85ab6eb57a9ab26743f6e6d1
Jason Rivera <58869791+jason-rivera@users.noreply.github.com>
2022-03-18 14:27:12 -0700

    updated navbar styling. no refresh on click

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,35 +1,37 @@
 import { Navbar, Container, Offcanvas, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 import styles from './NavBar.module.css';
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return(
-    <Navbar bg="dark" expand={false}>
+    <Navbar bg="dark" variant="dark" expand={false}>
       <Container fluid>
-        <Navbar.Brand href="#"></Navbar.Brand>
         <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        <Navbar.Brand href="#"></Navbar.Brand>
+        
         <Navbar.Offcanvas
           id="offcanvasNavbar"
           aria-labelledby="offcanvasNavbarLabel"
-          placement="end"
+          placement="start"
         >
-          <Offcanvas.Header closeButton>
+          <Offcanvas.Header className={styles.offCanvasHeader} closeButton>
             <Offcanvas.Title id="offcanvasNavbarLabel">Navigation</Offcanvas.Title>
           </Offcanvas.Header>
-          <Offcanvas.Body>
+          <Offcanvas.Body className={styles.offCanvasBody}>
             <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/about">About</Nav.Link>
-              <Nav.Link href="/movies">Movies</Nav.Link>
-              <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
+              <Nav.Link className={styles.navLink} as={Link} to="/">Home</Nav.Link>
+              <Nav.Link className={styles.navLink} as={Link} to="/about">About</Nav.Link>
+              <Nav.Link className={styles.navLink} as={Link} to="/movies">Movies</Nav.Link>
+              {/* <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
                 <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action5">
                   Something else here
                 </NavDropdown.Item>
-              </NavDropdown>
+              </NavDropdown> */}
             </Nav>
-            <Form className="d-flex">
+            {/* <Form className="d-flex">
               <FormControl
                 type="search"
                 placeholder="Search"
@@ -37,7 +39,7 @@ const NavBar = () => {
                 aria-label="Search"
               />
               <Button variant="outline-success">Search</Button>
-            </Form>
+            </Form> */}
           </Offcanvas.Body>
         </Navbar.Offcanvas>
       </Container>
